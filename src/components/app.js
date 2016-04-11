f9e2738dabf8510efb0e710575a082e5083a2aad
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-04-10 20:50:25 -0700

    Added starting point for row of board

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
+import BoardRow from '../containers/board-row';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React basic starter</div>
+      <BoardRow />
     );
   }
 }
